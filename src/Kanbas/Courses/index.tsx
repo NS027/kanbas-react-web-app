88067c672a59d3596f8ec6beaaa59a1957faf1d5
NS027 <chris_n89@hotmail.com>
2024-07-08 16:32:09 -0700

    adding Modules and Courses

---
+++ src/Kanbas/Courses/index.tsx
@@ -0,0 +1,29 @@
+import CoursesNavigation from "./Navigation";
+import Modules from "./Modules";
+import Home from "./Home";
+import { Navigate, Route, Routes } from "react-router";
+export default function Courses() {
+  return (
+    <div id="wd-courses">
+      <h2>Course 1234</h2>
+      <hr />
+      <table width="100%">
+        <tr>
+          <td valign="top">
+            <CoursesNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="Home" element={<h3>Home</h3>} />
+            <Route path="Modules" element={<Modules />} />
+            <Route path="Assignments"element={<h3>Assignments</h3>} />
+            <Route path="Assignments/:id"element={<h3>Assignment Editor</h3>} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+    </div>
+);}
+
+  
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,16 +1,21 @@
+import { Route, Routes } from "react-router";
+import Courses from "./Courses";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 
 export default function Kanbas() {
     return (
       <div id="wd-kanbas">
-        <table border={1}>
+        <table border={1} width="100%">
             <tr>
                 <td valign="top">
                     <KanbasNavigation />
                 </td>
                 <td valign="top">
-                    <Dashboard />
+                    <Routes>
+                        <Route path="/Courses/:id/*" element={<Courses />} />
+                        <Route path="Dashboard" element={<Dashboard />} />
+                    </Routes>
                 </td>
             </tr>
 
