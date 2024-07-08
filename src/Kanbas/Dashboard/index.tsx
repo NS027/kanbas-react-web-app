9d336d967421924a06884f23e4dc306b07424ee7
NS027 <chris_n89@hotmail.com>
2024-07-08 16:57:15 -0700

    adding six more courses

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -17,8 +17,76 @@ export default function Dashboard() {
               <a href="#/Kanbas/Courses/1234/Home"> Go </a>
             </div>
           </div>
-          <div className="wd-dashboard-course"> ... </div>
-          <div className="wd-dashboard-course"> ... </div>
+
+          <div className="wd-dashboard-course">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/5001/Home">
+                CS5001 Intensive Foundation of CS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Foundation of Computer Science
+              </p>
+              <a href="#/Kanbas/Courses/5001/Home"> Go </a>
+            </div>
+          </div>
+
+          <div className="wd-dashboard-course">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/5002/Home">
+                CS5002 Discrete Structure SEC
+              </a>
+              <p className="wd-dashboard-course-title">
+                Discrete Structure
+              </p>
+              <a href="#/Kanbas/Courses/5002/Home"> Go </a>
+            </div>
+          </div>
+
+          <div className="wd-dashboard-course">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/5004/Home">
+                CS5004 Objective-Oriented Design
+              </a>
+              <p className="wd-dashboard-course-title">
+                OOD with Java
+              </p>
+              <a href="#/Kanbas/Courses/5004/Home"> Go </a>
+            </div>
+          </div>
+
+          <div className="wd-dashboard-course">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/5008/Home">
+                CS5008 Data Str, Algo & App in CmpSys
+              </a>
+              <p className="wd-dashboard-course-title">
+                Data Structure and Algorithm in computer system
+              </p>
+              <a href="#/Kanbas/Courses/5008/Home"> Go </a>
+            </div>
+          </div>
+
+          <div className="wd-dashboard-course">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/5800/Home">
+                CS5800 Algorithm
+              </a>
+              <p className="wd-dashboard-course-title">
+                Master Level of Algorithm
+              </p>
+              <a href="#/Kanbas/Courses/5800/Home"> Go </a>
+            </div>
+          </div>
         </div>
       </div>
   );}
