993d1cb4bd7977cb822159eea506dd5ee7918a36
NS027 <chris_n89@hotmail.com>
2024-07-08 15:59:52 -0700

    initial adding dashbord with table align

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,9 +1,20 @@
+import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 
 export default function Kanbas() {
     return (
       <div id="wd-kanbas">
-        <KanbasNavigation />
+        <table border={1}>
+            <tr>
+                <td valign="top">
+                    <KanbasNavigation />
+                </td>
+                <td valign="top">
+                    <Dashboard />
+                </td>
+            </tr>
+
+        </table>
       </div>
   );}
   
