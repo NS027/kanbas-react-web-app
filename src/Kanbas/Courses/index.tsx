3aeb00c156efd70461589f3d43925fdc0bd3ef29
NS027 <chris_n89@hotmail.com>
2024-07-08 17:40:02 -0700

    adding assignments

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,6 +1,7 @@
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 import { Navigate, Route, Routes } from "react-router";
 export default function Courses() {
   return (
@@ -19,7 +20,7 @@ export default function Courses() {
               <Route path="Home" element={<Home />} />
             <Route path="Home" element={<h3>Home</h3>} />
             <Route path="Modules" element={<Modules />} />
-            <Route path="Assignments"element={<h3>Assignments</h3>} />
+            <Route path="Assignments"element={<Assignments />} />
             <Route path="Assignments/:id"element={<h3>Assignment Editor</h3>} />
             </Routes>
           </td>
