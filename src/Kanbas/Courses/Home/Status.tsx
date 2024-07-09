7973883869d07b33a64241261986a46ff2fa3919
NS027 <chris_n89@hotmail.com>
2024-07-08 17:34:17 -0700

    adding buttons on Home/status

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -4,6 +4,29 @@ export default function CourseStatus() {
         <h2>Course Status</h2>
         <button>Unpublish</button> <button>Publish</button>
         {/* Complete on your own */}
+        <table>
+          <tr>
+            <br />
+          </tr>
+          <tr>
+            <button id="wd-course-import-existing-content">Import Existing Content</button>
+          </tr>
+          <tr>
+            <button id="wd-course-import-from-commons">Import from Commons</button>
+          </tr>
+          <tr>
+            <button id="wd-course-choose-home-page">Choose Home Page</button>
+          </tr>
+          <tr>
+            <button id="wd-course-view-course-stream">View Course Stream</button>
+          </tr>
+          <tr>
+            <button id="wd-course-new-announcement">New Announcement</button>
+          </tr>
+          <tr>
+            <button id="wd-course-new-analytics">New Analytics</button>
+          </tr>
+        </table>
         <button>View Course Notifications</button>
       </div>
   );}
