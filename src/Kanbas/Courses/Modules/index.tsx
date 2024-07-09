fdffd34283d23836300df23db8f5580e734a7c49
NS027 <chris_n89@hotmail.com>
2024-07-08 17:20:37 -0700

    modify the module content

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -17,28 +17,55 @@ export default function Modules() {
             </td>
           </tr>
         </table>
-        <ul id="wd-modules">
-          <li className="wd-module">
-            <div className="wd-title">Week 1</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to the course</li>
-                  <li className="wd-content-item">Learn what is Web Development</li>
-                </ul>
+            <ul id="wd-modules">
+              <li id="wd-module">
+                  <div id="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
+                  <ul id="wd-lessons">
+                      <li id="wd-lesson">
+                          <span id="wd-title">LEARNING OBJECTIVES</span>
+                          <ul id="wd-content">
+                              <li id="wd-content-item">Introduction to the course</li>
+                              <li id="wd-content-item">Learn what is Web Development</li>
+                          </ul>
+                      </li>
+                      <li id="wd-lesson">
+                          <span id="wd-title">READING</span>
+                          <ul id="wd-content">
+                              <li id="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
+                              <li id="wd-content-item">Full Stack Developer - Chapter 2 - Creating User</li>
+                          </ul>
+                      </li>
+                      <li id="wd-lesson">
+                          <span id="wd-title">SLIDES</span>
+                          <ul id="wd-content">
+                              <li id="wd-content-item">Introduction to Web Development</li>
+                              <li id="wd-content-item">Creating an HTTP server with Node.js</li>
+                              <li id="wd-content-item">Creating a React Application</li>
+                          </ul>
+                      </li>
+                  </ul>
               </li>
-            </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 2</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
+              <li id="wd-module">
+                  <div id="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
+                  <ul id="wd-lessons">
+                      <li id="wd-lesson">
+                          <span id="wd-title">LEARNING OBJECTIVES</span>
+                          <ul id="wd-content">
+                              <li id="wd-content-item">Learn how to create user interfaces with HTML</li>
+                              <li id="wd-content-item">Deploy the assignment to Netlify</li>
+                          </ul>
+                      </li>
+                      <li id="wd-lesson">
+                          <span id="wd-title">SLIDES</span>
+                          <ul id="wd-content">
+                              <li id="wd-content-item">Introduction to HTML and the DOM</li>
+                              <li id="wd-content-item">Formatting Web content with Headings and</li>
+                              <li id="wd-content-item">Formatting content with Lists and Tables</li>
+                          </ul>
+                      </li>
+                  </ul>
               </li>
-            </ul>
-          </li>
-        </ul>
+          </ul>
       </div>
   );}
   
