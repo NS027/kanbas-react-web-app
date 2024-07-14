028199ec1b50ceb3c89c7c138e516710d729c384
NS027 <chris_n89@hotmail.com>
2024-07-14 10:25:27 -0700

    lab2 range and addons

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -541,6 +541,31 @@ export default function Lab2() {
         </div>
       </div>
 
+      <div id="wd-css-styling-range-and-sliders">
+        <h3>Range</h3>
+        <label htmlFor="range1" className="form-label">
+          Example range
+        </label>
+        <input type="range" className="form-range"
+          min="0" max="5" step="0.5" id="range1" />
+      </div>
+
+      <div id="wd-css-styling-addons">
+        <h3>Addons</h3>
+        <div className="input-group mb-3">
+          <span className="input-group-text">$</span>
+          <span className="input-group-text">0.00</span>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="input-group">
+          <input type="text" className="form-control" />
+          <span className="input-group-text">$</span>
+          <span className="input-group-text">0.00</span>
+        </div>
+      </div>
+
+
+
 
       </div>
     );
