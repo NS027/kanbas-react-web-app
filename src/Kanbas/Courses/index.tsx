317daf70dfd7711332844d8c7a030487cacefaeb
NS027 <chris_n89@hotmail.com>
2024-07-14 15:50:51 -0700

    modify the course navigation

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,12 +4,16 @@ import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editors";
 import { Navigate, Route, Routes } from "react-router";
+import { FaAlignJustify } from "react-icons/fa";
 export default function Courses() {
   return (
     <div id="wd-courses">
-      <h2>Course 1234</h2>
+      <h2 className="text-danger">
+        <FaAlignJustify className="me-4 fs-4 mb-1" />
+        Course 1234
+      </h2>
       <hr />
-      <table width="100%">
+      {/* <table width="100%">
         <tr>
           <td valign="top">
             <CoursesNavigation />
@@ -26,7 +30,16 @@ export default function Courses() {
             </Routes>
           </td>
         </tr>
-      </table>
+      </table> */}
+      <CoursesNavigation />
+      <div>
+        <Routes>
+          <Route path="Home" element={<Home />} />
+          <Route path="Modules" element={<Modules />} />
+          <Route path="Assignments" element={<Assignments />} />
+          <Route path="Assignments/:aid" element={<AssignmentEditor />} />
+        </Routes>
+      </div>
     </div>
 );}
 
