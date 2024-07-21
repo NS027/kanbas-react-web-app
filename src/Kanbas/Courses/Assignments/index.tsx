5b23a580f32ba77da896e61a540650a5904bf27c
NS027 <chris_n89@hotmail.com>
2024-07-21 14:00:04 -0700

    add comment to the modifiable json discription

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -1,94 +1,3 @@
-// import React from 'react';
-// import AssignmentControls from './AssignmentControls';
-// import AssignmentControlButtons from './AssignmentControlButtons';
-// import { BsGripVertical } from 'react-icons/bs';
-// import { IoEllipsisVertical } from 'react-icons/io5';
-// import { FaPlus } from 'react-icons/fa6';
-// import "./StyledBox.css";
-// import { AiOutlineFileText } from 'react-icons/ai';
-// import { useParams } from "react-router";
-// import * as db from "../../Database";
-// export default function Assignments() {
-//     const { cid } = useParams();
-//     const modules = db.modules;
-//     return (
-//       <div id="wd-assignments">
-
-//         <AssignmentControls /><br /><br /><br /><br />
-
-//         <ul id="wd-modules" className="list-group rounded-0">
-//             <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-//             {/* This is the title of the assignment */}
-//             <div className="wd-title p-3 ps-2 bg-secondary d-flex align-items-center justify-content-between">
-//               <div className="d-flex align-items-center">
-//                 <BsGripVertical className="me-2 fs-3" />
-//                 <span>ASSIGNMENT</span>
-//               </div>
-//               <div className="d-flex align-items-center">
-//                 <div className="styled-box me-2">
-//                   40% of Total
-//                 </div>
-//                 <FaPlus className="me-2" />
-//                 <IoEllipsisVertical className="fs-4" />
-//               </div>
-//             </div>
-
-//             <ul className="wd-assignments list-group rounded-0">
-//               <li className="wd-assignment list-group-item p-3 ps-1 d-flex align-items-center">
-//                 <BsGripVertical className="me-2 fs-3" />
-//                 <AiOutlineFileText className="me-2 fs-3" style={{ color: 'green' }} />
-//                 <div className="flex-grow-1">
-//                   <div className="fw-bold">
-//                     <a className="wd-assignment-link"
-//                       href="#/Kanbas/Courses/1234/Assignments/124">
-//                       A1
-//                     </a>
-//                     </div>
-//                   <div className="text-muted">
-//                     <span className="text-danger">Multiple Modules</span> | <span className="text-muted">Not available until May 6 at 12:00am</span> | Due May 13 at 11:59pm | 100 pts
-//                   </div>
-//                 </div>
-//                 <AssignmentControlButtons />
-//               </li>
-//               <li className="wd-assignment list-group-item p-3 ps-1 d-flex align-items-center">
-//                 <BsGripVertical className="me-2 fs-3" />
-//                 <AiOutlineFileText className="me-2 fs-3" style={{ color: 'green' }} />
-//                 <div className="flex-grow-1">
-//                   <div className="fw-bold">
-//                     <a className="wd-assignment-link"
-//                       href="#/Kanbas/Courses/1234/Assignments/124">
-//                       A2
-//                     </a>
-//                   </div>
-//                   <div className="text-muted">
-//                     <span className="text-danger">Multiple Modules</span> | <span className="text-muted">Not available until May 13 at 12:00am</span> | Due May 20 at 11:59pm | 100 pts
-//                   </div>
-//                 </div>
-//                 <AssignmentControlButtons />
-//               </li>
-//               <li className="wd-assignment list-group-item p-3 ps-1 d-flex align-items-center">
-//                 <BsGripVertical className="me-2 fs-3" />
-//                 <AiOutlineFileText className="me-2 fs-3" style={{ color: 'green' }} />
-//                 <div className="flex-grow-1">
-//                   <div className="fw-bold">
-//                     <a className="wd-assignment-link"
-//                       href="#/Kanbas/Courses/1234/Assignments/124">
-//                       A3
-//                     </a>
-//                   </div>
-//                   <div className="text-muted">
-//                     <span className="text-danger">Multiple Modules</span> | <span className="text-muted">Not available until May 20 at 12:00am</span> | Due May 27 at 11:59pm | 100 pts
-//                   </div>
-//                 </div>
-//                 <AssignmentControlButtons />
-//               </li>
-//             </ul>
-//             </li>
-//         </ul>
-//       </div>
-//     );
-// }
-
 import React from 'react';
 import AssignmentControls from './AssignmentControls';
 import AssignmentControlButtons from './AssignmentControlButtons';
@@ -139,6 +48,7 @@ export default function Assignments() {
                       </a>
                     </div>
                     <div className="text-muted">
+                      {/* All discription based on the modifiable json data */}
                       <span className="text-danger">Multiple Modules</span> | <span className="text-muted">{assignment.avaliable}</span> | {assignment.due} | {assignment.pts} pts
                     </div>
                   </div>
