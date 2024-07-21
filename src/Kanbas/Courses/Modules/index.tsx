663b4da7e9eec835052b51e941691af8a0faf769
NS027 <chris_n89@hotmail.com>
2024-07-21 16:35:11 -0700

    modify the module alginment

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -16,8 +16,8 @@ export default function Modules() {
             .map((module: any) => (
                 <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
                     <div className="wd-title p-3 ps-2 bg-secondary">
-                        <BsGripVertical className="me-2 fs-3" />
-                        {module.name}
+                        <span><BsGripVertical className="me-2 fs-3" />
+                        {module.name}</span>
                         <ModulesControlButtons />
                     </div>
                     {module.lessons && (
