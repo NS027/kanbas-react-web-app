31a3152eb1b002bb7104ceb6f873dac25c6ec6d0
NS027 <chris_n89@hotmail.com>
2024-07-21 10:03:57 -0700

    data driven course navigation

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,5 +1,8 @@
+import { Link, useLocation } from 'react-router-dom';
 import React, { useState } from 'react';
 import './index.css';
+import * as db from "../../Database";
+
 
 export default function CoursesNavigation() {
   const [activeLink, setActiveLink] = useState('Home');
@@ -8,65 +11,23 @@ export default function CoursesNavigation() {
     setActiveLink(link);
   };
 
+  const location = useLocation();
+  const course = db.courses[0];
+
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
   return (
     <div className="d-flex">
       <div id="wd-courses-navigation" className="list-group fs-5 rounded-0 me-3">
-        <a
-          id="wd-course-home-link"
-          href="#/Kanbas/Courses/1234/Home"
-          className={`list-group-item ${activeLink === 'Home' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Home')}
-        >
-          Home
-        </a>
-        <a
-          id="wd-course-modules-link"
-          href="#/Kanbas/Courses/1234/Modules"
-          className={`list-group-item ${activeLink === 'Modules' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Modules')}
-        >
-          Modules
-        </a>
-        <a
-          id="wd-course-piazza-link"
-          href="#/Kanbas/Courses/1234/Piazza"
-          className={`list-group-item ${activeLink === 'Piazza' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Piazza')}
-        >
-          Piazza
-        </a>
-        <a
-          id="wd-course-zoom-link"
-          href="#/Kanbas/Courses/1234/Zoom"
-          className={`list-group-item ${activeLink === 'Zoom' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Zoom')}
-        >
-          Zoom
-        </a>
-        <a
-          id="wd-course-quizzes-link"
-          href="#/Kanbas/Courses/1234/Assignments"
-          className={`list-group-item ${activeLink === 'Assignments' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Assignments')}
-        >
-          Assignments
-        </a>
-        <a
-          id="wd-course-assignments-link"
-          href="#/Kanbas/Courses/1234/Quizzes"
-          className={`list-group-item ${activeLink === 'Quizzes' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Quizzes')}
-        >
-          Quizzes
-        </a>
-        <a
-          id="wd-course-grades-link"
-          href="#/Kanbas/Courses/1234/Grades"
-          className={`list-group-item ${activeLink === 'Grades' ? 'active' : 'text-danger'} border border-0`}
-          onClick={() => handleClick('Grades')}
-        >
-          Grades
-        </a>
+        {links.map((link) => (
+          <Link
+            key={link}
+            to={`/Kanbas/Courses/${course._id}/${link}`}
+            className={`list-group-item ${activeLink === link ? 'active' : 'text-danger'} border border-0`}
+            onClick={() => handleClick(link)}
+          >
+            {link}
+          </Link>
+        ))}
       </div>
       <div className="content">
         {/* Content goes here */}
@@ -74,4 +35,4 @@ export default function CoursesNavigation() {
     </div>
   );
 }
-  
+
