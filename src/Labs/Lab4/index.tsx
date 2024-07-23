72e54457d3def1001f67ac96eb46e9d138b755d3
NS027 <chris_n89@hotmail.com>
2024-07-22 23:39:23 -0700

    Redux Examples

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,18 @@
+import React from 'react';
+import ReduxExamples from './ReduxExamples';
+import HelloRedux from './ReduxExamples/HelloRedux';
+import CounterRedux from './ReduxExamples/CounterRedux';
+import AddRedux from './ReduxExamples/AddRedux';
+import TodoList from './ReduxExamples/todos/TodoList';
 export default function Lab4() {
     return(
         <div id="wd-lab4" className="container-fluid">
-        
+            <h1>Lab 4</h1>
+            <TodoList />
+            <ReduxExamples />
+            <AddRedux />
+            <CounterRedux />
+            <HelloRedux />
         </div>
     );
 }
