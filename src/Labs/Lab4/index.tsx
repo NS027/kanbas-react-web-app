cc1abbb2a70ce5da4874144b701506206086ba76
NS027 <chris_n89@hotmail.com>
2024-07-26 18:32:14 -0700

    A4 Lab4 first half part of demo

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -4,7 +4,21 @@ import HelloRedux from './ReduxExamples/HelloRedux';
 import CounterRedux from './ReduxExamples/CounterRedux';
 import AddRedux from './ReduxExamples/AddRedux';
 import TodoList from './ReduxExamples/todos/TodoList';
+import ClickEvent from './ClickEvent';
+import PassingDataOnEvent from './PassingDataOnEvent';
+import PassingFunctions from './PassingFunctions';
+import EventObject from './EventObject';
+import Counter from './Counter';
+import BooleanStateVariables from './BooleanStateVariables';
+import StringStateVariables from './StringStateVariables';
+import DateStateVariable from './DateStateVariable';
+import ObjectStateVariable from './ObjectStateVariable';
+import ArrayStateVariable from './ArrayStateVariable';
+import ParentStateComponent from './ParentStateComponent';
 export default function Lab4() {
+    function sayHello() {
+        alert("Hello");
+    }
     return(
         <div id="wd-lab4" className="container-fluid">
             <h1>Lab 4</h1>
@@ -13,6 +27,19 @@ export default function Lab4() {
             <AddRedux />
             <CounterRedux />
             <HelloRedux />
+
+            <ObjectStateVariable />
+            <ArrayStateVariable />
+            <ParentStateComponent />
+            <ClickEvent />
+            <PassingDataOnEvent />
+            <PassingFunctions theFunction={sayHello} />
+            <EventObject />
+            <Counter />
+            <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+
         </div>
     );
 }
