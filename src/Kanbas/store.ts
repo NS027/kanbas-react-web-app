42a6b3b28089a98875e1b061555cfdae5c0f0b4d
NS027 <chris_n89@hotmail.com>
2024-08-12 11:12:36 -0700

    move Account out of Course

--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import assignmentsReducer from "./Courses/Assignments/assignmentsReducer";
-import accountReducer from "./Courses/Account/reducer";
+import accountReducer from "./Account/reducer";
 const store = configureStore({
     reducer: {
         modulesReducer,
