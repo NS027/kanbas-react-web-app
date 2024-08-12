402fe396263e1e90212b30e1dfa0292a41c26995
NS027 <chris_n89@hotmail.com>
2024-08-12 11:00:21 -0700

    modify the clients and signin

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,8 +3,6 @@ import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
-export interface User { _id: string; username: string; password: string; role: string;
-firstName: string, lastName: string };
 
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post( `${USERS_API}/signin`, credentials );
